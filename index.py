c16bd7fccec8aa5deb912122f33fb94faa08bb61
lrgingras <lrgingras@gmail.com>
2017-03-12 20:57:33 -0400

    Validation pour ajout

--- index.py
+++ index.py
@@ -19,31 +19,35 @@ from flask import render_template
 from flask import g
 from flask import request
 from flask import redirect
+import re
 from database import Database
 app = Flask(__name__)
 
-@app.route('/')
-def page_accueil():
-    limite='5'
-    articles = get_db().get_article_limite(limite)
-    return render_template('index.html', titre="Acceuil",  sous_titre="Pour les amoureux de la simplicité",articles=articles)
-
-
-@app.route('/article')
-def page_prog_web_avancee():
-    return render_template('about.html',titre="admin",sous_titre="")
-    
-@app.route('/recherche')
-def recherche():
-    valeur_recherche = 'es'
-    articles = get_db().get_recherche_article(valeur_recherche) 
-    return render_template('liste.html', articles=articles)
-    
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         g._database = Database()
     return g._database
+
+@app.route('/')
+def page_accueil():
+    limite='5'
+    articles = get_db().get_article_limite(limite)
+    return render_template('index.html', titre="Acceuil",
+                           sous_titre="Pour les amoureux de la simplicité",
+                           articles=articles)
+
+@app.route('/article')
+def page_prog_web_avancee():
+    return render_template('about.html',titre="admin",sous_titre="")
+
+@app.route('/recherche/<valeur_recherche>')
+def get_recherche_article(valeur_recherche):
+    articles = get_db().get_recherche_article(valeur_recherche)
+    if len(articles) == 0:
+        return render_template('aucun_article.html')
+    else:
+        return render_template('liste.html', articles=articles)
 
 
 @app.teardown_appcontext
@@ -52,35 +56,83 @@ def close_connection(exception):
     if db is not None:
         db.disconnect()
 
-
-@app.route('/deux-listes')
-def show_two_lists():
-    artists = get_db().get_artists()
-    albums = get_db().get_albums()
-    return render_template('2listes.html', artists=artists, albums=albums)
-
-
-@app.route('/vide')
-def show_two_empty_lists():
-    artists = []
-    albums = []
-    return render_template('2listes-vides.html', artists=artists, albums=albums)
-
-
-@app.route('/formulaire')
+@app.route('/admin-nouveau')
 def show_form():
     return render_template('form.html')
 
 
-@app.route('/new', methods=['POST'])
+@app.route('/ajout-article', methods=['POST'])
 def post_form():
-    name = request.form['nom']
-    if len(name) == 0:
-        return render_template('form.html', erreur='Le nom est obligatoire')
+    titre = request.form['titre']
+    identifiant = request.form['identifiant']
+    auteur = request.form['auteur']
+    date_publication = request.form['date_publication']
+    paragraphe = request.form['paragraphe']
+    valide = true
+    dict_validation = {}
+    # Validation du Titre
+    if len(titre) == 0:
+        dict_validation['titre'] = "Le titre est obligatoire."
+        valide = false
+    elif len(titre) > 100:
+        dict_validation['titre'] = "Le titre doit être d'un maximum de 100 caractère."
+        valide = false
     else:
-        get_db().insert_artist(name)
-        return redirect('/liste')
-        
+        dict_validation['titre'] = "Valide"
+    # Validation de l'Identifiant
+    if len(identifiant) == 0:
+        dict_validation['identifiant'] = "L'identifiant est obligatoire."
+        valide = false
+    elif len(identifiant) >= 50:
+        dict_validation['identifiant'] = "L'identifiant doit être d'un maximum de 50 caractère."
+        valide = false
+    elif re.match('[a-zA-Z_0-9]', identifiant):
+        dict_validation['identifiant'] = "L'identifiant ne doit utiliser que les caractères alphanumériques ainsi que le souligné(_)."
+        valide = false
+    else:
+        dict_validation['identifiant'] = 'Valide'
+    # Validation de l'Auteur
+    if len(auteur) == 0:
+        dict_validation['auteur'] = "L'auteur est obligatoire."
+        valide = false
+    elif len(auteur) > 100:
+        dict_validation['auteur'] = "L'auteur doit être d'un maximum de 100 caractère."
+        valide = false
+    else:
+        dict_validation['auteur'] = 'Valide'
+    # Validation du Paragraphe
+    if len(paragraphe) == 0:
+        dict_validation['paragraphe'] = "Le paragraphe est obligatoire."
+        valide = false
+    elif len(paragraphe) > 500:
+        dict_validation['paragraphe'] = "Le paragraphe doit être d'un maximum de 500 caractère."
+        valide = false
+    else:
+        dict_validation['paragraphe'] = 'Valide'
+    # Validation de la Date de Publication
+    if len(date_publication) == 10:
+        try:
+            datetime.datetime.strptime(date_text, '%Y-%m-%d')
+        except ValueError:
+            dict_validation['date_publication'] = "La date de publication n'est pas valide."
+            valide = false
+    # Traitement selon validation
+    if valide:
+        statut = get_db().set_nouvel_article_admin(titre, identifiant, auteur,
+                                                  date_publication, paragraphe)
+        if statut == 0:
+            return redirect('/admin')
+        else:
+            return render_template('erreur_de_mise_a_jour.html')
+    else:
+        response = make_response(render_template('admin-nouveau.html', 
+                                                 erreur=dict_validation))
+        response.set_cookie("nom", nom)
+        response.set_cookie("prenom", prenom)
+        response.set_cookie("identifiant", identifiant)
+        response.set_cookie("date_validation", date_validation)
+        response.set_cookie("paragraphe", paragraphe)
+        return response
 
 if __name__ == '__main__':
     app.run(debug=True)
